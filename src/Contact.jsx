f749b8f669cd87365b2cc700ba027bd2b3daa0c0
kyash99252 <yashkadam99252@gmail.com>
2024-01-15 19:44:20 +0530

    CSS isn't Daijobu. Will fix that soon

---
+++ src/Contact.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import styles from "./style";
+import "./idx.css";
+import {
+  Navbar,
+  Hero,
+  Stats,
+  Research,
+  Billing,
+  CardDeal,
+  Testimonials,
+  Clients,
+  CTA,
+  Query,
+  Footer,
+} from "./component";
+
+const Contact = () => {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Query />
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./index.css";
-import App from "./App";
 import { ContextProvider } from "./contexts/ContextProvider";
+import Dashboard from "./Dashboard";
 
 ReactDOM.render(
   <ContextProvider>
-    <App />
+    <Dashboard />
   </ContextProvider>,
   document.getElementById("root")
 );
